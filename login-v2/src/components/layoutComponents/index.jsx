f6e580fdd779a1699432607746b64b8665215bc9
Andre da Silva Carmona <104648375+DevCarmona@users.noreply.github.com>
2024-02-29 14:37:40 -0300

    Criando tela de cadastro

---
+++ login-v2/src/components/layoutComponents/index.jsx
@@ -0,0 +1,13 @@
+
+
+export const LayoutComponents = () => {
+  return (
+    <div className="container">
+      <div className="container-login">
+        <div className="wrap-login">
+
+        </div>
+      </div>
+    </div>
+  );
+};
